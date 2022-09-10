d56382e5e96afc33d7688850b0b5ba272a12afbb
Star <111043921+PranavShilavane@users.noreply.github.com>
2022-09-10 10:50:26 +0530

    Create nonStaticVariables1.cpp
    C++

---
+++ nonStaticVariables1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class A
+{
+public:
+    int a;
+};
+
+int main()
+{
+    A s;
+    A d;
+
+    s.a = 111;
+    d.a = 777;
+
+    cout << s.a << endl;
+    cout << d.a << endl;
+
+    return 0;
+}
